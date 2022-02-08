36abbff968f2279525142f67fc71bf945a0b30b8
Kalashnikkov <jcsayalak7@gmail.com>
2022-02-08 17:30:32 +1300

    Splitting APIs to returning two different types of data

--- pages/api/getRevenue.js
+++ pages/api/getRevenue.js
@@ -1,11 +1,11 @@
-import { orders } from "../../src/data";
+import { revenueThisWeek } from "../../src/data";
 
 export default function handler(req, res) {
     // Don't need a request - keep it simple. Would like to authenticate a user is able to retrieve data
     try {
-        res.status(200).json({orders})
+        res.status(200).json({revenueThisWeek})
     } catch (err) {
-        res.status(500).json({error: 'Failed to retreieve orders.'})
+        res.status(500).json({error: 'Failed to retreieve revenue.'})
     }
   }
   
