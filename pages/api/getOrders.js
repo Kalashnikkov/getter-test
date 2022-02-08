0d579efb548ff2cabf04a33fae52e7a5964b15e7
Kalashnikkov <jcsayalak7@gmail.com>
2022-02-08 17:29:23 +1300

    Add error to fetch

--- pages/api/getOrders.js
+++ pages/api/getOrders.js
@@ -1,6 +1,11 @@
 import { orders, revenueThisWeek } from "../../src/data";
 
 export default function handler(req, res) {
-    res.status(200).json({orders, revenueThisWeek})
+    // Don't need a request - keep it simple. Would like to authenticate a user is able to retrieve data
+    try {
+        res.status(200).json({orders, revenueThisWeek})
+    } catch (err) {
+        res.status(500).json({error: 'Failed to retreieve data.'})
+    }
   }
   
