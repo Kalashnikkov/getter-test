864530803705964260b830b692ab4e8926d32d51
Kalashnikkov <jcsayalak7@gmail.com>
2022-02-08 17:48:23 +1300

    Fixing a few stuff

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 const test = require('ava');
 
-import orderHandler from './pages/api/getOrders';
-import revenueHandler from './pages/api/getRevenue';
-
-
 test.todo('should return correct data from getOrder');
 test.todo('should return correct data from getRevenue');
 test.todo('should return correct response if failed');
