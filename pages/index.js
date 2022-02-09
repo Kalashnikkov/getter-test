3c108e8e553e18b5b8217fc5fc2f8f27c2ebd23f
Kalashnikkov <jcsayalak7@gmail.com>
2022-02-09 15:08:03 +1300

    Tailwind CSS Styling

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Link href="/sales-dashboard">
-        <a>Click here to go to the dashboard.</a>
-      </Link>
+    <div class="bg-gray-200 h-screen flex items-center justify-center">
+      <div class="px-5 flex flex-col">
+        <h2 class="text-gray-900">
+          <div class="text-4xl block font-extrabold">Johathan's Dashboard</div>
+          <div class="text-xl block text-green-600 mt-1 italic">Using NextJS, TailwindCSS, ChartJS, and Ava</div>
+        </h2>
+        <div class="flex items-center">
+          <div class="my-3">
+            <Link href="/sales-dashboard">
+              <button class="px-4 py-2 border border-transparent font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
+                View Dashboard
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
